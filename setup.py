785b32ab9f533f5c38fa14309e53a1a2578a3135
Alice Vitti Kageyama Ferreira <alicevkf@gmail.com>
2024-10-23 20:06:01 -0300

    Adiciona e edita requirements/setup; remove antigo template de pré-processamento

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GP_comparative_analysis',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'seaborn',
+        'matplotlib',
+        'numpy',
+        'scikit-learn',
+        'torch',
+        'gpytorch',
+        'pyro-ppl',
+        'optuna',
+        'codecarbon',
+        'ucimlrepo'
+    ],
+    extras_require={
+        'dev': [
+            'jupyter',
+            'pytest'
+        ]
+    },
+)
+
+# pode estar errado!
